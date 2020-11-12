837598702d6a23812ac16df486f178ee9bb8b4ef
T.Bardachova <t.bardachova@physik.uni-muenchen.de>
2020-11-12 14:25:17 +0100

    Add initialization of physical constants

--- main.cpp
+++ main.cpp
@@ -28,16 +28,43 @@ void heating(vector<double> &theta, const double &dp, const double &E_abs,
   return;
 }
 
+/* Initialization of physical constants */
+//'Python' way
+struct con_struct 
+{
+    const double kappa = 2.0 / 7.0; // adiabatic exponent [/]
+    const double c_air = 1004; // specific heat capacity [J/kg K]
+    const double g = 9.80665; // gravity acceleration [m/s^2]
+    const double E_abs = 235; // heating rate from surface [W/m^2]   
+};
+
+/*
+// 'C++' way
+class con 
+{
+// declaration
+public:
+    static const double kappa; // adiabatic exponent [/]
+    static const double c_air; // specific heat capacity [J/kg K]
+    static const double g; // gravity acceleration [m/s^2]
+    static const double E_abs; // heating rate from surface [W/m^2]
+};
+// initialization
+const double con::kappa = 2.0 / 7.0;
+const double con::c_air = 1004;
+const double con::g = 9.80665; 
+const double con::E_abs = 235;
+*/
+
 int main() {
+    
+  struct con_struct con; // make an object of struct in 'main' for 'Python' way
+    
   int nlayer = 25; // number of layers
-  double kappa = 2.0 / 7.0; // adiabatic exponent [/]
-  double c_air = 1004; // specific heat capacity [J/kg K]
-  double g = 9.80665; // gravity acceleration [m/s^2]
   double dt = 360.0; // time step length [s]
   double n_steps = 1000; // number of timesteps [/]
   int output_steps = 100; // Intervall in which the model produces output [/]
-  double E_abs = 235; // heating rate from surface [W/m^2]
-
+  
   double dp = 1000.0 / (double) nlayer;
   double dT = 100.0 / (double) nlayer;
 
@@ -48,17 +75,18 @@ int main() {
   vector<double> theta(nlayer); // vector of pot. temperatures for each layer
   vector<double> t_to_theta(nlayer); // vector for the conversion factors between t and theta
 
+  /* define pressure levels */  
   for (int i=0; i<nlevel; i++) {
     p[i] = dp * (double) i; // the pressure levels are spaced equally between 0 and 1000 hPa
   }
 
+  /* define T, p and theta for each layer */
   for (int i=0; i<nlayer; i++) {
     T[i] = 180.0 + dT * (double) i; // just a first guess for the T-profile for each layer
     player[i] = (p[i]+p[i+1])/2.0; // computation of pressure between the levels
-    t_to_theta[i] = pow(1000.0 / player[i], kappa); // computation of conversion factors
+    t_to_theta[i] = pow(1000.0 / player[i], con.kappa); // computation of conversion factors
     theta[i] = T[i] * t_to_theta[i]; // computation of theta for each layer
   }
-
 
   /* end of initialization */
 
@@ -79,7 +107,7 @@ int main() {
       output(time, player, T, theta);
     }
 
-    heating(theta, dp, E_abs, dt, g, c_air, t_to_theta);
+    heating(theta, dp, con.E_abs, dt, con.g, con.c_air, t_to_theta);
   }
 
   return 0;
